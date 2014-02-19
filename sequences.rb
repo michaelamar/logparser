8ff0bbe2b8644d4e5b363a66ac37c05670778fc2
Michael Amar <Mordachiamar@gmail.com>
2014-02-19 15:51:49 -0500

    oop log parser almost done

--- sequences.rb
+++ sequences.rb
@@ -4,79 +4,46 @@ class Sequences
 
 	def initialize(pages)
 		@sequences=[] #of sequence objects
-		
 		@pages=pages.pages
+		@ending = 3					
+		@increment = 2					
+							
 		#print @pages
 		#print @pages.pages
 		# count_sequences()
-		@ending = 3
-		@increment = 2
-
 	end
 
 
 
-	# def process_objects(pages_object)
-	# 			# pages_object.pages.each do |user_page_object|
-	# 			# 	puts user_page_object.pages
-	# 			# end
-	# end
-
-
-	# def count_user_pages(user_pages)
-	# 	@number_of_objects_to_process = user_pages.pages.length
-	# end
-
-
-	# def create_sequences(sequence, )
-		
-	# end
-
-
-	# def check_if_sequence_exists_yet(sequence)
-		
-	# end
 
 	def first_method
-		for i in (1..(@pages.length))		#method 1
-			if (@pages[i].nil? == false)	#method 1
-				s=@pages[i]
-				second_method(s)					#method 1
+		for i in (1..(@pages.length))	
+			if (@pages[i].nil? == false)
+				pages_list=@pages[i]
+				second_method(pages_list)				
 			end
 		end
 	end
 
 	def second_method(list_of_pages)
-		x=0
+		count=0
 		range = 0
-		while (x <= (list_of_pages.pages.length-@ending))						##method 2
-	 		sequence =	list_of_pages.pages[range..(range+@increment)].join('-')	##method 2
-			z=Sequence.new(sequence)								##method 2
+		while (count <= (list_of_pages.pages.length-@ending))						
+	 		sequence =	list_of_pages.pages[range..(range+@increment)].join('-')	
+			z=Sequence.new(sequence)								
 			third_method(z)
-			x=x+1													##method 2
+			count=count+1													
 			range = range+1
 		end
-
 	end
 
 	def third_method(z)
-		# for i in (1..(@pages.length))		#method 1
-		# 	if (@pages[i].nil? == false)	#method 1
-		# 		s=@pages[i]					#method 1
-		# 		x=0							#method 1
-		# 		range=0						#method 1
-		# 		while (x <= (s.pages.length-@ending))						##method 2
-		# 	 		sequence =	s.pages[range..(range+@increment)].join('-')	##method 2
-		# 			z=Sequence.new(sequence)								##method 2
-		 			if @sequences.include? z			##method 3
-			 	 		increment_counts(z)				##method 3
-			 		else								##method 3
-			 	 		z.increment_count				##method 3
-						@sequences << z					##method 3
-			 		end 														##method 2	
-				
-										##method 1
-										##method 1
+		if @sequences.include? z			
+	 		increment_counts(z)				
+		else								
+	 		z.increment_count				
+			@sequences << z					
+		end
 	end
 
 	def increment_counts(sequence)
@@ -85,21 +52,11 @@ class Sequences
 		x.increment_count
 	end
 
-	# def find_length_of_user_pages(pages)
-	# 	pages.length
-	# end
-
-
-	# def check_if_nil()
-		
-	# end
-
 
 
 
 	def print_out_answer
 	 	print @sequences.sort_by!{|x| x.count}
-
 	end
 
 
